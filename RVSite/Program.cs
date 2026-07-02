f51179fe62a7a6d62a033d566096c294aaa07563
XaRodriguezWSU <xanderrodriguez@mail.weber.edu>
2026-07-01 20:35:48 -0600

    Fix database discrepencies and data seeding

--- RVSite/Program.cs
+++ RVSite/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddControllersWithViews();
 if (builder.Environment.IsDevelopment())
 {
     builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+        options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 }
 else
 {
@@ -31,7 +31,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+    db.Database.EnsureCreated();
 }
 
 
@@ -39,6 +39,10 @@ using (var scope = app.Services.CreateScope())
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    db.Sites.RemoveRange(db.Sites);
+    db.SaveChanges();
+
     if (!db.Sites.Any())
     {
         db.Sites.AddRange(
