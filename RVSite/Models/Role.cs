40190540dc3768b06f06e780fe4ab6890a47f45d
Lindsey West <westlindsey01@gmail.com>
2026-07-01 19:51:37 -0600

    added Role.cs, RoleType, and User.cs models

---
+++ RVSite/Models/Role.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RVSite.Models
+{
+    public class Role
+    {
+        [Key]
+        public int RoleID { get; set; }
+
+        [Required]
+        public RoleType Type { get; set; }
+
+        public ICollection<User> Users { get; set; } = new List<User>();
+
+        public bool CanSubmitStatusChange()
+        {
+            return Type == RoleType.Admin || Type == RoleType.Staff;
+        }
+
+        public string GetRoleName()
+        {
+            return Type.ToString();
+        }
+    }
+}
---
+++ RVSite/Models/User.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RVSite.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required]
+        public int RoleID { get; set; }
+
+        public Role? Role { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
+
+
+        public bool HasRole(string roleName)
+        {
+            return Role?.RoleName == roleName;
+        }
+    }
+}
