b907ab3619dea4000701d1c68b5d24c74cddc308
XaRodriguezWSU <xanderrodriguez@mail.weber.edu>
2026-07-05 11:01:29 -0600

    Update database context and add database data seeder.

--- RVSite/Models/AppDbContext.cs
+++ RVSite/Models/AppDbContext.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RVSite.Models;
 
-namespace SiteDemo.Models
+namespace RVSite.Models
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Site> Sites { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Role { get; set; }
     }
 }
